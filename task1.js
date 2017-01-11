78ac35288dd7caec14bb044966d7567abd17183a
Andreas Hirsch <s1519002046@students.fh-hagenberg.at>
2017-01-11 16:55:09 +0100

    refactored old version

--- task1.js
+++ task1.js
@@ -1,60 +1,54 @@
 var width = 1000,
-        height = 1000;
+    height = 1000;
 
-    // Creates sources <svg> element
-        const svg = d3.select("body").append("svg")
-        .attr("width", width)
-        .attr("height", height);
+// Creates sources <svg> element
+const svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
 
-        var projection = d3.geo.mercator()
-        .scale(1800) // scales your map
-        .translate([width * 4, height *1.8]); // centers in SVG
+//scale map
+var projection = d3.geo.mercator()
+    .scale(2300)
+    .translate([width * 5, height * 2.2]);
 
-        var path = d3.geo.path()
-       .projection(projection);
+//generate path
+var path = d3.geo.path()
+    .projection(projection);
 
-       d3.json("/data/us.json", function(json) { // loads JSON file
-      svg.selectAll("path") // selects path elements, will make them if they don't exist
-       .data(json.features) // iterates over geo feature
-       .enter() // adds feature if it doesn't exist as an element
-       .append("path") // defines element as a path
-       .attr("d", path) // path generator translates geo data to SVG
-    });
+d3.json("/data/us.json", (json) => {
+    svg.selectAll("path")
+        .data(json.features)
+        .enter().append("path")
+        .attr("d", path)
+});
 
+// Global variable for all data
+var data;
 
+d3.csv('/data/SacramentocrimeJanuary2006-2.csv',  (error, csv) => {
+    data = csv;
 
-    // Global variable for all data
-    var data;
+    update(data);
+});
 
-    d3.csv('/data/SacramentocrimeJanuary2006-2.csv', (d) => {
-      return {
-        latitude: d.latitude,
-        longitude: d.longitude
-      };
-    }, (error, csv) => {
-        data = csv;
+function update(new_data) {
 
-        update(data);
-    });
+    // DATA JOIN
+    const rect = svg.selectAll('rect').data(new_data);
 
-    function update(new_data) {
+    // ENTER
+    // new elements
+    const rect_enter = rect.enter().append("circle", "rect")
+        .attr("r", 1)
+        .attr("fill", "red")
+        .attr("transform", (d) => {
+            return "translate(" + projection([
+                d.longitude,
+                d.latitude
+            ]) + ")";
+        });
+    // EXIT
+    // elements that aren't associated with data
+    rect.exit().remove();
 
-        // DATA JOIN
-        const rect = svg.selectAll('rect').data(new_data);
-
-        // ENTER
-        // new elements
-        const rect_enter = rect.enter().append("circle", "rect")
-            .attr("r", 1)
-            .attr("fill", "red")
-            .attr("transform", function(d) {
-                return "translate(" + projection([
-                    d.longitude,
-                    d.latitude
-                ]) + ")";
-            });
-        // EXIT
-        // elements that aren't associated with data
-        rect.exit().remove();
-
-    }
+}
